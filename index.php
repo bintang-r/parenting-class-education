9d5825658a4fee34e64a1418df334a9ec759213a
M-BintangR <muhbintang650@gmail.com>
2025-05-17 18:05:41 +0800

    Update index.php to redirect directly to /src/index.php on direct access

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
 
-require_once __DIR__ . '/config.php';
-require_once __DIR__ . '/src/index.php';
+// Redirect ke /src/index.php jika file ini diakses langsung
+header('Location: src/index.php');
+exit;
