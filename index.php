1f82a6dcd8b265a168bc5909293d5285ff0facff
M-BintangR <muhbintang650@gmail.com>
2025-05-18 21:26:20 +0800

    Implement routing logic for redirecting to appropriate pages based on request URI

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php
 
-// Redirect ke /src/index.php jika file ini diakses langsung
-header('Location: src/index.php');
+$request = $_SERVER['REQUEST_URI'];
+$path = parse_url($request, PHP_URL_PATH);
+
+// Ambil bagian terakhir dari path
+$route = basename($path);
+
+switch ($route) {
+     case 'login':
+          header('Location: src/login.php');
+          break;
+     case 'dashboard':
+          header('Location: src/dashboard/index.php');
+          break;
+     default:
+          header('Location: src/index.php');
+          break;
+}
 exit;
