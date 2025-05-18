28b47a033ae638b95dc7ada0e1af9b68be8d9c0e
M-BintangR <muhbintang650@gmail.com>
2025-05-18 22:31:47 +0800

    little change

--- index.php
+++ index.php
@@ -1,20 +1,5 @@
 <?php
 
-$request = $_SERVER['REQUEST_URI'];
-$path = parse_url($request, PHP_URL_PATH);
-
-// Ambil bagian terakhir dari path
-$route = basename($path);
-
-switch ($route) {
-     case 'login':
-          header('Location: src/login.php');
-          break;
-     case 'dashboard':
-          header('Location: src/dashboard/index.php');
-          break;
-     default:
-          header('Location: src/index.php');
-          break;
-}
+// Redirect ke /src/index.php jika file ini diakses langsung
+header('Location: src/index.php');
 exit;
